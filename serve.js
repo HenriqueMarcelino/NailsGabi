232dcc7c4ad5f8ac693281b7fca0acf63bf80af4
Henrique Marcelino <henrique.marcelino1996@gmail.com>
2021-02-13 21:33:20 -0300

    Atualização do Serve.js

--- serve.js
+++ serve.js
@@ -11,7 +11,7 @@ app.get("/", function(req, res) {
   // criar o index.ejs dentro da pasta views
 })
 
-app.get('/produtos',function(req,res){
+app.get('/home',function(req,res){
     res.render("home/contato");
 });
 
