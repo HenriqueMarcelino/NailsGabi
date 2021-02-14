93353cc5ed416808ef59525a2a65f7fa36bbabe2
Henrique Marcelino <henrique.marcelino1996@gmail.com>
2021-02-14 10:47:31 -0300

    Modificação no Serve.js

--- serve.js
+++ serve.js
@@ -1,20 +1,10 @@
-var express = require('express');
-var app = express();
+var express = requere('express')
+var path = require('path')
+var serveStatic = require('serve-static')
 
-app.set('view engine','ejs');
+var app = express()
+app.use(serveStatic(path.join(__dirname)))
 
-app.get("/", function(req, res) {
-    res.send("Respota da Home");
-   // Você pode trocar a linha acima por:
-  // res.render('"index")
-  // lembre que pra funcionar a linha acima você tem que 
-  // criar o index.ejs dentro da pasta views
-})
-
-app.get('/home',function(req,res){
-    res.render("home/contato");
-});
-
-app.listen(3000,function(){
-    console.log("servidor rodando");
-});
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('serve started' + port)
